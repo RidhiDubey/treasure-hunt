dc4159e2c8385c691d5121856ff3a56475bc5b3a
RidhiDubey <76939922+RidhiDubey@users.noreply.github.com>
2021-05-16 22:52:41 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+    textSize(15)
+    text("F U N C T I O N ", 700,150)
+    fill("lightblue")
+    text("Hint: essential in oop", 700,170)
+
+    fill("white")
+textSize(15)
+text("D A T A B A S E", 400,300)
+fill("lightblue")
+text("Hint: Stores info !!", 400,330)
+
+}
